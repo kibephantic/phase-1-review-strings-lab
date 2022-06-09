65f6d6e0752f063ff1936624e97ad44eae6b6c8c
PatrickKibe <phantickibe01@gmail.com>
2022-06-09 16:49:44 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+//const WelcomeMessage = 'Welcome to Flatbook,'+currentUser;
+const welcomeMessage = 'WELCOME TO FLATBOOK,${Grace Hopper}!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+//const shortGreeting ="";
+//const shortGreeting = 'Welcome,';
+//const shortGreetiing = 'Welcome, ${currentUser}!';
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
